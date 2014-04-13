8dd4268865801f683a49fe309e8472ec069182c3
preinh <preinh@gmail.com>
2014-04-13 09:01:09 -0300

    add multiprocessing pool

--- hmtk/seismicity/smoothing/helmstetter2012.py
+++ hmtk/seismicity/smoothing/helmstetter2012.py
@@ -11,6 +11,7 @@ from hmtk.seismicity.smoothing.kernels import gaussian
 from hmtk.seismicity.utils import haversine
 from matplotlib import pylab as pl
 
+from multiprocessing import Pool
 
 class smoothing(object):
     
@@ -122,15 +123,20 @@ class smoothing(object):
         k = k if (k >= 1) else 1
         _d, _i = tree.query([0.,0.], k=np.round(k))
         return p[1] - np.max(  X[_i, 1]  )
+
+
     
-    def coupled_nearest_neighbor(self, X, k, a, tree):
+    def coupled_nearest_neighbor(self, X, k, a, i):
+        tree = spatial.KDTree(X)
         h, d = optimize.fmin_slsqp(self._cnn_model, [10., 10.], 
                                    args=(X, k, a, tree),
                                    ieqcons=[self._cnn_time_constraint, 
                                             self._cnn_space_constraint],
                                    full_output=False,
                                    iprint=False)
-        return h, d
+        return i, h, d
+
+    
     #### END: couple nearest neighbor analysis
 
     def optimize_catalogue_bandwidths(self, k, a, plotting=False):
@@ -142,8 +148,18 @@ class smoothing(object):
         X,Y,Z,M,T = catalogue_to_XYZMT(catalogue)
         D = haversine(X, Y, X, Y)
         
-        h, d = [], []
+        h = np.zeros(len(X))
+        d = np.zeros(len(X))
         
+        po = Pool()
+
+        def cb_cnn(r):
+            i, hi, di = r[0], r[1], r[2]   
+            #print i, hi, di
+            d[i] = di
+            h[i] = hi
+
+        #print h
         #for each earthquake i
         for i, v in enumerate(zip(D, T)):
             # variable init
@@ -158,25 +174,30 @@ class smoothing(object):
             _i = distances <= 1.0
             distances[_i] = 1.0
             
-            # condense vector [t, r], but exclude exclude the calculation point
+            # condensed vector [t, r], but exclude exclude the calculation point
             TD = np.array( zip( days.ravel(), distances.ravel() ) )[1:]
 
             # compute nearest neighbor tree
-            tree = spatial.KDTree(TD)
             
             # compute each [h, d] by CNN 
-            hi, di = self.coupled_nearest_neighbor(TD, k, a, tree)
+            po.apply_async(wrap_cnn, (self, TD, k, a, i, ), callback=cb_cnn)
+            #hi, di = self.coupled_nearest_neighbor(TD, k, a, tree)
         
-            if self.plot_bandwidth:
-                pl.scatter(days, distances, s=50, c='y', marker='x', alpha=0.7)
-                pl.ylim(ymin=0)
-                pl.axvline(hi)
-                pl.axhline(di)
-                pl.show()
+#             if self.plot_bandwidth:
+#                 pl.scatter(days, distances, s=50, c='y', marker='x', alpha=0.7)
+#                 pl.ylim(ymin=0)
+#                 pl.axvline(hi)
+#                 pl.axhline(di)
+#                 pl.show()
+#  
+#             d.append(di)
+#             h.append(hi)
 
-            d.append(di)
-            h.append(hi)
+        po.close()
+        po.join()
 
+        #print h
+        
         # after process each one, 
         # store optimized h and d for given catalog
         catalogue.data['h'] = np.array(h)
@@ -220,7 +241,7 @@ class smoothing(object):
         pass
 
     
-    def seismicity_rate(self, r, t, r_min, k, a):
+    def seismicity_rate(self, r, t, r_min, k, a, i):
         # get LEARNING catalogue
         catalogue = self.learning_catalogue
         
@@ -257,21 +278,26 @@ class smoothing(object):
         ####  K  E  R  N  E  L
         # compute kernels and normalization factor
         time_kernel = gaussian.Kernel()  
+#        print "k1"
         K1 = time_kernel.value(days, h)
         
         distance_kernel = gaussian.Kernel()
+#        print "k2"
         K2 = distance_kernel.value(distances, d)
 
         ####  W  E  I  G  H  T
+#        print "norm"
         w = catalogue.data['w'][_i]
         norm = (2. * w ) / (h * d * d)
 
         # compute seismicity_rate
         # K2 is bi-dimensional
+#        print "rates"
         rates = np.array([ r_min + sum(norm * K1 * K2) for K2 in K2 ]) 
         rates[ (rates <= 0) ] = r_min
+#        print "end"
 
-        return rates
+        return i, rates
 
 
     def stationary_rate_model(self, r, t, r_min, k, a, normalized=True):
@@ -283,7 +309,21 @@ class smoothing(object):
         # seismicity_rate timeseries on each cell grid
         
         if self.log: print "getting smoothed seismicity seismicity_rate timeseries"
-        rates = [ self.seismicity_rate(r, _t, r_min=r_min, k=k, a=a) for _t in t ]
+        
+        rates = np.zeros( (len(t), len(r)) )
+        po = Pool()
+        
+        def cb_sr(r):
+            i, rate = r[0], r[1]
+            rates[i,:] = rate
+
+        for i, _t in enumerate(t):
+            po.apply_async(wrap_seismicity_rate, (self, r, _t, r_min, k, a, i,), 
+                           callback=cb_sr) 
+        
+        po.close()
+        po.join()
+        
         rates = np.array(rates).T # one timeseries array for each grid point
         
         if self.plot_rate_timeseries:
@@ -291,7 +331,7 @@ class smoothing(object):
                 pl.plot(t, _y,'g-')
                 pl.scatter(t, _y, c='g', marker='+')
                 pl.axhline(np.median(_y), c='r')
-                pl.title('Stationary Modeled Seismicity Rate')
+                pl.title('Stationary Modeled Seismicity Rate in one cell')
                 pl.show()
     
         # get de median of rates distribution
@@ -303,18 +343,35 @@ class smoothing(object):
  
         if self.plot_stationary_rate:       
             #from mpl_toolkits.mplot3d import axes3d as p3
-            l = self.grid_limits
-            dx = l['xspc']
-            dy = l['yspc']
-            
-            _X = np.arange(l['xmin'], l['xmax'] + dx, dx) 
-            _Y = np.arange(l['ymin'], l['ymax'] + dy, dy) 
-            _x, _y = np.meshgrid(_X, _Y)
+#             l = self.grid_limits
+#             dx = l['xspc']
+#             dy = l['yspc']
+#             
+#             _X = np.arange(l['xmin'], l['xmax'] + dx, dx) 
+#             _Y = np.arange(l['ymin'], l['ymax'] + dy, dy) 
+#             _x, _y = np.meshgrid(_X, _Y)
             _x, _y = r[:,0], r[:,1]
             #plpmeshcolor()
             pl.scatter(_x, _y, c=rates, cmap=pl.cm.get_cmap('RdYlGn_r'), marker='s', alpha=0.7)
             pl.colorbar()
+            pl.title('Stationary Seismicity Rate')
             pl.show()
+            
+            import csv
+            fid = open('stationary_rate_bsb2013.csv', 'wt')
+            # Create header list
+            header_info = ['Longitude', 'Latitude', 'Rate']
+            writer = csv.DictWriter(fid, fieldnames=header_info)
+            headers = dict((name0, name0) for name0 in header_info)
+            # Write to file
+            writer.writerow(headers)
+            for (__x, __y, __r) in zip(_x, _y, rates):
+                row_dict = {'Longitude': '%.5f' % __x,
+                            'Latitude': '%.5f' % __y,
+                            'Rate': '%.3f' % __r}
+                writer.writerow(row_dict)
+            fid.close()
+
 
         if self.log: print "reporting parameters: r_min=%.4f, k=%d, a=%d" % (r_min, k, a)
 
@@ -385,7 +442,6 @@ class smoothing(object):
         r_min = parameters[2]
         k = parameters[1]
         a = parameters[0]
-        
         Np = self.stationary_rate_model(self.r, self.t, r_min, k, a)
         n = self.observed_number_of_events()
 
@@ -396,28 +452,51 @@ class smoothing(object):
 
         # negative likelihood
         NLL = np.sum(np.log10( p ))
+        
         if self.log: print "likelyhood evaluation: ", NLL
         return NLL
 
 
     def optimize_seismicity_model(self):
         
-        r_constraint = lambda p: p[2] - 0.001
-        k_constraint = lambda p: p[1] - 1.5
-        a_constraint = lambda p: p[0] - 1.5
- 
-        if self.log: print "optimizing model"
-    
-        # Run the minimizer
-        initial_parameters = [10, 10, 0.001]
-        results = optimize.fmin_cobyla(s.negative_log_likelihood,
-                                       initial_parameters,
-                                       cons=[a_constraint,
-                                             k_constraint,
-                                             r_constraint],
-                                       rhobeg = 50.0,
-                                       rhoend = 1,
-                                       maxfun = 100)
+        from openopt import MINLP
+        f = s.negative_log_likelihood
+        
+        x0=[1, 1, 1] # a, k, r_min
+        p = MINLP(f, x0, maxIter = 1e3)
+
+        p.lb = [  0,   1,  0]
+        p.ub = [500, 100,  1]
+
+        p.contol = 1.1e-6
+
+        p.name = 'minlp_1'
+        nlpSolver = 'ipopt'
+        
+        # coords of discrete variables and sets of allowed values
+        #p.discreteVars = {0:range(0, int(1e3)), 1:range(1, int(2e2))}
+        
+        # required tolerance for discrete variables, default 10^-5
+        p.discrtol = 1.1e-5
+        
+        results = p.solve('branb', nlpSolver=nlpSolver, plot = True)
+
+#         r_constraint = lambda p: p[2] - 0.001
+#         k_constraint = lambda p: p[1] - 1.5
+#         a_constraint = lambda p: p[0] - 1.5
+#  
+#         if self.log: print "optimizing model"
+#     
+#         # Run the minimizer
+#         
+#         results = optimize.fmin_cobyla(s.negative_log_likelihood,
+#                                        initial_parameters,
+#                                        cons=[a_constraint,
+#                                              k_constraint,
+#                                              r_constraint],
+#                                        rhobeg = 50.0,
+#                                        rhoend = 1,
+#                                        maxfun = 100)
                     
         # Print the results. They should be really close to your actual values
         return results
@@ -440,6 +519,13 @@ class smoothing(object):
 ## TODO:
     # gain
     # tests
+
+def wrap_cnn(*karg, **kwarg):
+    return karg[0].coupled_nearest_neighbor(*karg[1:], **kwarg)
+
+def wrap_seismicity_rate(*karg, **kwarg):
+    return karg[0].seismicity_rate(*karg[1:], **kwarg)
+
 
 
 class completeness(object):
@@ -532,7 +618,7 @@ if __name__ == '__main__':
               'completeness_table' : comp_table,
               'b_value' : 1.0, 
               'stationary_time_step_in_days': 365,
-              'catalogue_year_start': 1900,
+              'catalogue_year_start': 1950,
               'catalogue_year_divisor': 2000,
               'target_minimum_magnitude': 3.5,
               'add_before_learning_on_target': True,
@@ -551,4 +637,69 @@ if __name__ == '__main__':
 #     s.plot_catalogue(s.target_catalogue, title="Target Catalogue U \ ]1960-2000[")
 
     print s.optimize_seismicity_model()
+#     s.plot_stationary_rate = True
+#     s.stationary_rate_model(s.r, s.t, r_min=0.0010, k=29, a=165)
+    
+#     
+#     
+# """
+# Example of MINLP
+# It is recommended to read help(NLP) before
+# and /examples/nlp_1.py 
+# """
+# from openopt import MINLP
+# from numpy import *
+# 
+# N = 150
+# K = 50
+# 
+# #objective function:
+# f = lambda x: ((x-5.45)**2).sum()
+# 
+# #optional: 1st derivatives
+# df = lambda x: 2*(x-5.45)
+# 
+# # start point
+# x0 = 8*cos(arange(N))
+# 
+# # assign prob:
+# # 1st arg - objective function
+# # 2nd arg - start point
+# # for more details see 
+# # http://openopt.org/Assignment 
+# p = MINLP(f, x0, df=df, maxIter = 1e3)
+# 
+# # optional: set some box constraints lb <= x <= ub
+# p.lb = [-6.5]*N
+# p.ub = [6.5]*N
+# # see help(NLP) for handling of other constraints: 
+# # Ax<=b, Aeq x = beq, c(x) <= 0, h(x) = 0
+# # see also /examples/nlp_1.py
+# 
+# # required tolerance for smooth constraints, default 1e-6
+# p.contol = 1.1e-6
+# 
+# p.name = 'minlp_1'
+# 
+# # required field: nlpSolver - should be capable of handling box-bounds at least
+# #nlpSolver = 'ralg' 
+# nlpSolver = 'ipopt'
+# 
+# # coords of discrete variables and sets of allowed values
+# p.discreteVars = {7:range(3, 10), 8:range(3, 10), 9:[2, 3.1, 9]}
+# 
+# # required tolerance for discrete variables, default 10^-5
+# p.discrtol = 1.1e-5
+# 
+# #optional: check derivatives, you could use p.checkdc(), p.checkdh() for constraints
+# #p.checkdf()
+# 
+# # optional: maxTime, maxCPUTime
+# # p.maxTime = 15
+# # p.maxCPUTime = 15
+# 
+# r = p.solve('branb', nlpSolver=nlpSolver, plot = False)
+# # optim point and value are r.xf and r.ff,
+# # see http://openopt.org/OOFrameworkDoc#Result_structure for more details
 
+#     
